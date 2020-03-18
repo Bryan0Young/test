f6ba3567144cffa4edb7014981f2ffe3ff6bb8d9
unknown <asdo129@qq.com>
2020-03-18 08:55:09 +0800

    '初始项目'

---
+++ WebApplication2/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index(IndexModel model)
+        {
+            
+            model.Result = model.Num1 + model.Num2;
+            return View(model);
+        }
+    }
+}
